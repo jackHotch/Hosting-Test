13c664a5811764d44bec7294e0d96444d0e8192a
jackHotch <jhotchkiss2021@gmail.com>
2024-07-02 20:47:28 -0400

    added headers in cors

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ import { getDriver } from './database.js'
 
 app.use(express.json())
 app.use(cors({
-  origin: 'https://hosting-test-client.onrender.com'
+  origin: 'https://hosting-test-client.onrender.com',
+  headers: {
+    "Access-Control-Allow-Origin": "https://hosting-test-client.onrender.com",
+    "Access-Control-Allow-Credentials": true
+},
 }))
 
 app.get('/driver/:number', async (req, res) => {
